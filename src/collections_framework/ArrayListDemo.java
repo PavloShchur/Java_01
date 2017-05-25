54e627b94e7685fb8dc7781292eb7bb982fcb04b
Pavlo <pavloshchur@gmail.com>
2017-05-25 11:56:58 +0300

    Java_01

---
+++ src/collections_framework/ArrayListDemo.java
@@ -0,0 +1,27 @@
+package collections_framework;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		System.out.println("Initial size of al: " + al.size());
+		
+		al.add("C");
+		al.add("A");
+		al.add("E");
+		al.add("B");
+		al.add("D");
+		al.add("F");
+		al.add(1, "A2");
+		System.out.println("Size of al after addtions: " + al.size());
+		System.out.println("Contents of al: " + al);
+		al.remove("F");
+		al.remove(2);
+		System.out.println("Sizr of al after deletions: " + al.size());
+		System.out.println("Contents of al: " + al);
+	}
+
+}
